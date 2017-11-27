945ed8c24603ed87b3c9792a5b584bddafd21d1c
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-11-27 18:10:48 +0700

    fix

--- Web/DataAccess/Classes/tblAccount.cs
+++ Web/DataAccess/Classes/tblAccount.cs
@@ -12,8 +12,7 @@ namespace DataAccess.Classes
         public string Email { get; set; }
 
         public string Password { get; set; }
-
-        public bool? RemeberPassword { get; set; }
+        
 
         public int Type { get; set; }
 
@@ -28,6 +27,21 @@ namespace DataAccess.Classes
 
         public bool? Status { get; set; }
 
-        
+        public static List<tblAccount> Get_All_Accounts()
+        {
+            return CBO.FillCollection<tblAccount>(DataProvider.Instance.ExecuteReader("SP_Select_tblAccount"));
+        }
+
+        public static tblAccount Get_Accounts_By_Email(string email)
+        {
+            return CBO.FillObject<tblAccount>(DataProvider.Instance.ExecuteReader("SP_Select_tblAccount_By_Email", email));
+        }
+
+        public static DataSet Check_Login(string email, string pass)
+        {
+
+            return DataProvider.Instance.ExecuteDataset("SP_Login", email, pass);
+            //DataProvider.Instance.ExecuteReader("SP_Login", email, pass);
+        }
     }
 }
--- Web/NhaHang/Admin/Login.aspx.cs
+++ Web/NhaHang/Admin/Login.aspx.cs
@@ -9,6 +9,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Configuration;
+using DataAccess.Classes;
+using DataAccess;
 
 public partial class Admin_Login : System.Web.UI.Page
 {
@@ -23,94 +25,41 @@ public partial class Admin_Login : System.Web.UI.Page
     
     protected void btnLogin_Click(object sender, EventArgs e)
 	{
-		KiemTraNhap(txtEmail.Text + "", MaHoapass(txtPassword.Text).Trim());
+		KiemTraNhap(txtEmail.Text + "", Common.MaHoapass(txtPassword.Text).Trim());
 	}
 
-	private void KiemTraNhap(string email, string pass)
-	{
-		DataTable dtb = StoreToDataTable(email, pass);
-		int num = 0;
-		if (dtb.Rows.Count > 0)
-		{
-			num = int.Parse("0" + dtb.Rows[0][0]);
-			switch (num)
-			{
-				case 0: // Khai báo Session cho phép đăng nhập
-					Session["email"] = txtEmail.Text.ToString().ToLower();
-					Session["pass"] = MaHoapass(txtPassword.Text);
-					Response.Redirect("Default.aspx");
-					break;
-				case 1: //Thông báo tên đăng nhập không tồn tại
-					ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Lỗi: Tên đăng nhập không tồn tại');", true);
-					break;
-				case 2: // thông báo sai mật khẩu
-					ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Lỗi; Mật khẩu đăng nhập không đúng!');", true);
-					break;
-				case 3: //thông báo email đã bị khóa
-					ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Lỗi: Tên đăng nhập này đã bị khóa!');", true);
-					break;
-				case 4: // Thông báo phải đổi mật khẩu và chuyển người dùng đến trang Doipass.aspx
-					ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Bạn phải đổi mật khẩu trước khi đăng nhập!');location.href='Doipass.aspx?userid=" + email + "'", true);
-					break;
-			}
-		}
-		dtb.Dispose();
-	}
-	public string MaHoapass(string password)
-	{
-		MD5 md5 = new MD5CryptoServiceProvider();
+    private void KiemTraNhap(string email, string pass)
+    {
+        DataTable dt = tblAccount.Check_Login(email, pass).Tables[0];
+        int num = 0;
+        if (dt.Rows.Count > 0)
+        {
+            num = int.Parse("0" + dt.Rows[0][0]);
+            switch (num)
+            {
+                case 0: // Khai báo Session cho phép đăng nhập
+                    tblAccount account = tblAccount.Get_Accounts_By_Email(email);
+                    Session["email"] = account.Email;
+                    Session["type"] = account.Type;
+                    Session["name"] = account.Name;
+                    Session["avatar"] = account.Avatar;
 
-		//Compute hash from the bytes of text
-		md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password));
-
-		//Get hash result after compute it
-		byte[] result = md5.Hash;
-
-		StringBuilder strBuilder = new StringBuilder();
-		for (int i = 0; i < result.Length; i++)
-		{
-			//Change it into 2 hexadecimal digits
-			//For each byte
-			strBuilder.Append(result[i].ToString("x2"));
-		}
-
-		return strBuilder.ToString();
-	}
-	private static DataSet ThucThiStore_DataSet(string StoredProcedure, params SqlParameter[] Parameters)
-	{
-	    string ConnectionString = ConfigurationManager.ConnectionStrings["NhaHangConnectionString"].ConnectionString; ;// @"Data Source=.\SQLEXPRESS;Initial Catalog=NhaHang;Integrated Security=True";
-		SqlConnection Conn = new SqlConnection(ConnectionString);
-		SqlCommand Command = new SqlCommand(StoredProcedure, Conn);
-		if (Parameters != null)
-		{
-			Command.Parameters.Clear();
-			Command.Parameters.AddRange(Parameters);
-		}
-		DataSet ds = new DataSet();
-		SqlDataAdapter da = new SqlDataAdapter(StoredProcedure, Conn);
-		Command.CommandType = CommandType.StoredProcedure;
-		da.SelectCommand = Command;
-		try
-		{
-			Conn.Open();
-			da.Fill(ds);
-		}
-		finally
-		{
-			if (Conn.State == ConnectionState.Open)
-				Conn.Close();
-			Conn.Dispose();
-		}
-		return ds;
-	}
-	private DataTable StoreToDataTable(string email, string pass)
-	{
-		SqlParameter[] arrParam = {
-            new SqlParameter("@Email", SqlDbType.NVarChar),
-            new SqlParameter("@Password", SqlDbType.NVarChar)
-            };
-		arrParam[0].Value = email;
-		arrParam[1].Value = pass;
-		return ThucThiStore_DataSet("SP_Login", arrParam).Tables[0];
-	}
+                    Response.Redirect("Default.aspx");
+                    break;
+                case 1: //Thông báo tên đăng nhập không tồn tại
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Lỗi: Tên đăng nhập không tồn tại');", true);
+                    break;
+                case 2: // thông báo sai mật khẩu
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Lỗi; Mật khẩu đăng nhập không đúng!');", true);
+                    break;
+                case 3: //thông báo email đã bị khóa
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Lỗi: Tên đăng nhập này đã bị khóa!');", true);
+                    break;
+                case 4: // Thông báo phải đổi mật khẩu và chuyển người dùng đến trang Doipass.aspx
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "redirectMe", "alert('Bạn phải đổi mật khẩu trước khi đăng nhập!');location.href='Doipass.aspx?userid=" + email + "'", true);
+                    break;
+            }
+        }
+        dt.Dispose();
+    }
 }
