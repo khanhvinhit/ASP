aeafb5202dcad339ea5cc752b77a1ff2767e96ba
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-11-27 20:28:51 +0700

    fix

--- Web/DataAccess/Classes/tblOrder.cs
+++ Web/DataAccess/Classes/tblOrder.cs
@@ -16,7 +16,10 @@ namespace DataAccess.Classes
         public int? AccountID { get; set; }
 
         public DateTime? CreateDate { get; set; }
-        
 
+        public static List<tblOrder> Get_All()
+        {
+            return CBO.FillCollection<tblOrder>(DataProvider.Instance.ExecuteReader("SP_Select_Order"));
+        }
     }
 }
