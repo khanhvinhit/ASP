913a441d9ddac56718b2ff063815b09c66c00cfb
Sang Khánh Vinh <khanhvinhit@gmail.com>
2017-11-28 09:03:22 +0700

    ok

---
+++ Web/NhaHang/Admin/About.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataAccess.Classes;
+
+public partial class Admin_About : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["email"] == null)
+        {
+            Response.Redirect("Login.aspx");
+        }
+        else
+        {
+            if (!IsPostBack)
+            {
+                PopulateControls();
+            }
+        }
+    }
+
+    private void PopulateControls()
+    {
+        ListTable.Controls.Clear();
+        ListTable.DataSource = tblAbout.All();
+        ListTable.DataBind();
+    }
+}
