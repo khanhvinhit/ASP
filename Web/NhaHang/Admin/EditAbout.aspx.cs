ebfd03f0eb71e5000b360b457e4ab1b5efded007
nnvncg <Congnvctk37@gmail.com>
2017-12-16 13:40:46 +0700

    dfh

--- Web/NhaHang/Admin/EditAbout.aspx.cs
+++ Web/NhaHang/Admin/EditAbout.aspx.cs
@@ -11,7 +11,7 @@ public partial class Admin_EditAbout : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["email"] != null && Session["typeID"].GetHashCode() == 1)
+        if (Session["email"] == null && Session["typeID"].GetHashCode() != 1)
         {
             Response.Redirect("Login.aspx");
         }
